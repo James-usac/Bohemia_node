fdc36981847bf6c7f71c278725ba497e6d6716e8
James-usac <2811874520101@ingenieria.usac.edu.gt>
2020-07-11 19:23:47 -0600

    haciendo modificacion
    Modificando test

---
+++ red-bicicletas/spec/models/usuario_test.spec.js
@@ -0,0 +1,73 @@
+var Bicicleta = require('../../models/bicicleta');
+var mongoose = require('mongoose');
+var Reserva = require('../../models/reserva');
+var Usuario = require('../../models/usuario');
+
+describe('Probando usuarios ...', function(){
+    beforeEach(function(done){
+        
+        var mongoDB = 'mongodb://localhost/testdb';
+        mongoose.connect(mongoDB,
+            {
+                useNewUrlParser : true,
+                useUnifiedTopology: true
+            });
+        
+        var db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection error:')); 
+        db.once('open', function() {
+            console.log('Conectado a la base de datos de test database');
+            done();
+        });
+    });
+
+    afterEach(function(done){
+        Reserva.deleteMany({},function(err,success){
+            if(err)
+                console.log(err);
+            Usuario.deleteMany({},function(err,success){
+                if(err)
+                    console.log(err);
+                Bicicleta.deleteMany({},function(err,success){
+                    if(err)
+                        console.log(err);
+                    done();
+                });
+            });    
+        });
+    });
+
+    describe('Al momento de reservar',() => {
+
+        Mailer.sendEmail();
+        const codeP = 3890;
+        it('debe existir la reserva',(done) => {
+            const usuario = new Usuario({nombre : 'Edison Lopera'});
+            usuario.save();
+            
+            const bicicleta = new Bicicleta({code : codeP, color : 'Borgona', modelo : "Super bike"}); 
+            bicicleta.save();
+
+            var hoy = new Date();
+            var manana = new Date();
+            manana.setDate(hoy.getDate() + 1);
+
+            usuario.reservar(bicicleta.id, hoy, manana,function(err, reserva){
+                
+                Reserva.find({}).populate('bicicleta').populate('usuario').exec(function(err,res){
+                    if(err)
+                        console.log(err);
+                    console.log(res);
+                    var cont = res[0];
+                    console.log(cont);
+                    expect(res.length).toBe(1);
+                    expect(cont.diasDeReserva()).toBe(2);
+                    expect(cont.bicicleta.code).toBe(codeP);
+                    expect(cont.usuario.nombre).toBe(usuario.nombre);
+                    done();
+                });
+            });
+        })
+    });
+
+});
